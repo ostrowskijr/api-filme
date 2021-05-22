7536da30d321d24f8cdd8a6a051180344110f276
Luis Antonio Ostrowski Junior <ostrowskijr@gmail.com>
2021-05-22 20:35:40 -0300

    Final Project.

--- src/main/java/br/com/ostrowskijr/apimovies/data/CollectionWinners.java
+++ src/main/java/br/com/ostrowskijr/apimovies/data/CollectionWinners.java
@@ -40,29 +40,33 @@ public class CollectionWinners {
                     Optional<Winner> optional = winners.stream().filter(winner -> winner.getProducer().equals(producer))
                             .findAny();
                     if (!optional.isPresent()) {
-                        try {
-                            // Filtrar o movies que o produtor consta
-                            List<Movie> producers = filterMovies.stream()
-                                    .filter(m -> m.getProducers().contains(producer)).collect(Collectors.toList());
-                            // Pegar o menor ano e maior ano de premio do produtor.
-                            Movie min = producers.stream().min(Comparator.comparing(Movie::getYear)).get();
-                            // Pergar o próximo prêmio maior que o anterior.                                                
-                            producers.clear();
-                            producers = filterMovies.stream()                            
-                                    .filter(m -> m.getProducers().contains(producer) && m.getYear() > min.getYear())
-                                    .collect(Collectors.toList());
-                            int lastYear = min.getYear();
-                            if (!producers.isEmpty()){
-                                Movie max = producers.stream().min(Comparator.comparing(Movie::getYear)).get();
-                                //
-                                lastYear = max.getYear();
-                            }                            
-                            // Calcular o Intervalo entre os prêmios
-                            int interval = lastYear - min.getYear();
-                            Winner winner = new Winner(producer, interval, min.getYear(), lastYear);
-                            winners.add(winner);
-                        } catch (Exception e) {                            
-                            e.printStackTrace();
+                        List<Integer> dates = filterMovies.stream().filter(m -> m.getProducers().contains(producer))
+                                .map(Movie::getYear).collect(Collectors.toList());
+                        if (dates.size() > 1) {
+                            System.out.println(
+                                    producer + " - Years: " + dates.toString() + " Total Wins: " + dates.size());
+                            try {
+                                int firstYear = 0;
+                                int lastYear = 0;
+                                int interval = 0;
+                                for (int index = 0; index < dates.size(); index++) {
+                                    if (dates.size() >= index + 1) {
+                                        if (firstYear == 0) {
+                                            firstYear = dates.get(index);
+                                        } else {
+                                            firstYear = lastYear;
+                                        }
+                                        lastYear = dates.get(index);
+                                        // Calcular o Intervalo entre os prêmios
+                                        interval = lastYear - firstYear;
+                                        Winner winner = new Winner(producer, interval, firstYear, lastYear);
+                                        winners.add(winner);
+                                    }
+                                }
+
+                            } catch (Exception e) {
+                                e.printStackTrace();
+                            }
                         }
                     }
                 }
@@ -79,24 +83,27 @@ public class CollectionWinners {
         JsonObject json = new JsonObject();
         JsonArray min = new JsonArray();
         JsonArray max = new JsonArray();
-        // Filtrar apenas produtores com intervalado de prêmio maior que 0, para descartar produtores com apenas 1 prêmio.
+        // Filtrar apenas produtores com intervalado de prêmio maior que 0, para
+        // descartar produtores com apenas 1 prêmio.
         List<Winner> winnerSort = winners.stream().filter(w -> w.getInterval() > 0).collect(Collectors.toList());
         // Ordernar do menor para o maior intevalo do prêmio
         winnerSort.sort(Comparator.comparing(Winner::getInterval));
-        // Pegar os dois primeiros produtores que conseguiran conquistar 2 prêmios em menor tempo.
+        // Pegar os dois primeiros produtores que conseguiran conquistar 2 prêmios em
+        // menor tempo.
         Winner w = winnerSort.get(0);
         min.add(createJsonObject(w.getProducer(), w.getInterval(), w.getPreviousWin(), w.getFollowingWin()));
         w = winnerSort.get(1);
         min.add(createJsonObject(w.getProducer(), w.getInterval(), w.getPreviousWin(), w.getFollowingWin()));
         json.add("min", min);
-        // Pegar os dois últimos produtores que conseguiran conquistar 2 prêmios em maior tempo.
-        int lastWin = winnerSort.size() - 1;        
+        // Pegar os dois últimos produtores que conseguiran conquistar 2 prêmios em
+        // maior tempo.
+        int lastWin = winnerSort.size() - 1;
         w = winnerSort.get(lastWin - 1);
         max.add(createJsonObject(w.getProducer(), w.getInterval(), w.getPreviousWin(), w.getFollowingWin()));
         w = winnerSort.get(lastWin);
         max.add(createJsonObject(w.getProducer(), w.getInterval(), w.getPreviousWin(), w.getFollowingWin()));
         json.add("max", max);
-        //        
+        //
         return json.toString();
     }
 
